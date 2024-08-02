0bd640a185a3bcb0b139811ac80320274444766d
Daniil Bastrich <bastrich+github@pm.me>
2024-08-02 23:07:54 +0200

    Refactor and prepare to submission

--- sim/parallel_sim.py
+++ sim/parallel_sim.py
@@ -1,30 +1,31 @@
 from sim.sim import Sim
 from concurrent.futures import ProcessPoolExecutor
-from queue import Queue
+from multiprocessing import Manager
 
 
 class ParallelSim:
     def __init__(self, mode, parallelization):
-        self.sims = Queue(maxsize=parallelization)
+        self.sims = Manager().Queue(maxsize=parallelization)
         for _ in range(parallelization):
             self.sims.put(Sim(mode, False))
 
         self.sims_executor = ProcessPoolExecutor(max_workers=parallelization)
 
     def run(self, walking_strategies, number_of_steps=1000):
-        futures = [self.sims_executor.submit(self.simulate, walking_strategy, number_of_steps) for walking_strategy in walking_strategies]
+        futures = [self.sims_executor.submit(simulate, walking_strategy, self.sims, number_of_steps) for walking_strategy in walking_strategies]
         return [future.result() for future in futures]
-
-    def simulate(self, walking_strategy, number_of_steps):
-        sim = self.sims.get()
-        fitness, steps = sim.run(walking_strategy, number_of_steps)
-        self.sims.put(sim)
-
-        return {
-            'walking_strategy': walking_strategy,
-            'fitness': fitness,
-            'steps': steps
-        }
 
     def __del__(self):
         self.sims_executor.shutdown()
+
+
+def simulate(walking_strategy, sims, number_of_steps):
+    sim = sims.get()
+    fitness, steps = sim.run(walking_strategy, number_of_steps)
+    sims.put(sim)
+
+    return {
+        'walking_strategy': walking_strategy,
+        'fitness': fitness,
+        'steps': steps
+    }
--- visualize.py
+++ visualize.py
@@ -1,48 +1,12 @@
-
 import pickle
-from sim import Sim
-
-# visualize the best
-# with open('best-0', 'rb') as file:
-#     best_walking_strategy = pickle.load(file)
+from sim.sim import Sim
 
 with open('results/population', 'rb') as file:
     population = pickle.load(file)
-#
-# fitness_values = np.array([ws.evaluated_fitness for ws in population.walking_strategies])
-# min_fitness = np.min(fitness_values)
-# if min_fitness < 0:
-#     fitness_values = fitness_values + np.abs(min_fitness)
-#
-# fit_map = fitness_values / np.sum(fitness_values)
-#
-# parent1, parent2 = np.random.choice(population.walking_strategies, size=2, p=fit_map)
-#
-# best_walking_strategy = parent1.crossover(parent2)
-
-# with open(f'test', 'wb') as file:
-#     pickle.dump(best_walking_strategy, file)
-
-# with open('test', 'rb') as file:
-#     best_walking_strategy = pickle.load(file)
-#
-# #
-# best_walking_strategy = best_walking_strategy.mutate(0.3, 0.3).mutate(0.3, 0.3).mutate(0.3, 0.3).mutate(0.3, 0.3).mutate(0.3, 0.3)
 
 best_walking_strategy = population.walking_strategies[0]
 
-# activations = np.array([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) / 100
-# # activations = np.repeat([80], 40) / 100
-# best_walking_strategy.muscles[0] = Muscle(period=200, fourier_coefficients=np.fft.fft(activations)[:5])
-
-
-
-# best_walking_strategy = best_walking_strategy.mutate(0.3, 0.3).mutate(0.3, 0.3).mutate(0.3, 0.3).mutate(0.3, 0.3).mutate(0.3, 0.3)
-
-# best_walking_strategy.change_precision(5)
-
 sim = Sim(mode='2D', visualize=True)
-total_reward, sim_steps = sim.run(best_walking_strategy)
+fitness, sim_steps = sim.run(best_walking_strategy)
 
-print(f'{total_reward} for {sim_steps} steps')
-print(f'expected {best_walking_strategy.evaluated_fitness}')
+print(f'{fitness} for {sim_steps} steps')
