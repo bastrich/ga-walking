e359f6a0428e052a2b75567e2732dd00e2c2e6de
Daniil Bastrich <bastrich+github@pm.me>
2024-08-23 01:42:00 +0200

    Add more unit tests

--- tests/unit_tests/test_muscle.py
+++ tests/unit_tests/test_muscle.py
@@ -1,41 +1,50 @@
 import unittest
-from unittest.mock import patch, call
+import numpy as np
 from walking_strategy.muscle import Muscle
 
 
 class TestMuscle(unittest.TestCase):
 
-    def test_genotype_validation(self):
-        self.assertRaises(ValueError, Muscle, 200, '4D')
+    def test_validation(self):
+        self.assertRaises(ValueError, Muscle, period=200, mode='4D')
+        self.assertRaises(ValueError, Muscle, period=123, mode='2D')
+        self.assertRaises(ValueError, Muscle, period=200, mode='2D', type='asd')
+        self.assertRaises(ValueError, Muscle, period=200, mode='2D', sampling_interval=456)
+        self.assertRaises(ValueError, Muscle, period=200, mode='2D', precision=1)
+        self.assertRaises(ValueError, Muscle, period=200, mode='2D', components=[123])
+        self.assertRaises(ValueError, Muscle, period=200, mode='2D', sampling_interval=40, precision=10)
 
-    # @patch('numpy.random.choice')
-    # @patch.object(Muscle, 'generate_random_activations_dna')
-    # @patch.object(Muscle, 'calculate_fourier_activations')
-    def test_random_init(self):
-    # def test_random_init(self, mock_calculate_fourier_activations, mock_generate_random_activations_dna, mock_choice):
-        period = 120
-        discretization_frequency = 40
-        activations_format = 'fourier'
-        activations_dna = [1, 2, 3]
-        muscle_activations = [4, 5, 6]
+    def test_components_and_activations(self):
+        muscle = Muscle(period=400, mode='2D', sampling_interval=20, precision=10)
+        self.assertEqual(10, len(muscle.components))
+        self.assertEqual(400, len(muscle.activations))
 
-        # mock_choice.side_effect = [discretization_frequency, activations_format]
-        # mock_generate_random_activations_dna.return_value = activations_dna
-        # mock_calculate_fourier_activations.return_value = muscle_activations
+        muscle = Muscle(period=200, mode='2D', sampling_interval=10, precision=5)
+        self.assertEqual(5, len(muscle.components))
+        self.assertEqual(200, len(muscle.activations))
 
-        muscle = Muscle(period, '2D')
+    def test_change_period(self):
+        muscle = Muscle(period=400, mode='2D')
+        muscle = muscle.with_period(240)
+        self.assertEqual(240, len(muscle.activations))
 
-        # self.assertEqual(muscle.discretization_frequency, discretization_frequency)
-        # self.assertEqual(muscle.activations_format, activations_format)
-        # self.assertEqual(muscle.activations_dna, activations_dna)
-        # self.assertEqual(muscle.muscle_activations, muscle_activations)
-        # self.assertEqual(
-        #     mock_choice.call_args_list,
-        #     [call([5, 10, 20, 40]), call(['direct', 'fourier'])]
-        # )
-        # mock_generate_random_activations_dna.assert_called_once_with(period, activations_format, discretization_frequency)
-        # mock_calculate_fourier_activations.assert_called_once_with(activations_dna, period)
+        muscle = Muscle(period=160, mode='2D')
+        muscle = muscle.with_period(320)
+        self.assertEqual(320, len(muscle.activations))
 
+    def test_mutate_components(self):
+        muscle = Muscle(period=200, mode='2D', type='direct')
+        new_muscle = muscle.mutate_components(1, 1)
+        for i in range(muscle.precision):
+            self.assertTrue(muscle.components[i] != new_muscle.components[i] or muscle.components[i] == 0 or muscle.components[i] == 1)
+            self.assertIsInstance(new_muscle.components[i], float)
+            self.assertTrue(abs(new_muscle.components[i] - muscle.components[i]) < 0.5)
 
-# if __name__ == '__main__':
-#     unittest.main()
+        muscle = Muscle(period=200, mode='2D', type='fourier')
+        new_muscle = muscle.mutate_components(1, 1)
+        for i in range(muscle.precision):
+            self.assertNotEqual(muscle.components[i], new_muscle.components[i])
+            self.assertIsInstance(new_muscle.components[i], complex)
+            self.assertTrue(abs(abs(new_muscle.components[i]) - abs(muscle.components[i])) < muscle.period / muscle.sampling_interval)
+            print(abs(np.angle(new_muscle.components[i]) - np.angle(muscle.components[i])))
+            self.assertTrue(abs(np.angle(new_muscle.components[i]) - np.angle(muscle.components[i])) < 6)
